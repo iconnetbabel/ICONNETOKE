4143d99e863203580a5899e928cd96931eca615a
iconnetbabel <grimzy1142@gmail.com>
2024-07-18 09:00:28 +0700

    Update metode.js

--- metode.js
+++ metode.js
@@ -1,11 +1,11 @@
 function showPaymentMethod(methodNumber) {
-    // Sembunyikan semua metode pembayaran
+    // Hide all payment methods
     var paymentMethods = document.querySelectorAll('.payment-method');
     paymentMethods.forEach(function(method) {
         method.classList.add('hidden');
     });
 
-    // Tampilkan metode pembayaran yang dipilih
+    // Show the selected payment method
     var selectedMethod = document.getElementById('paymentMethod' + methodNumber);
     if (selectedMethod) {
         selectedMethod.classList.remove('hidden');
